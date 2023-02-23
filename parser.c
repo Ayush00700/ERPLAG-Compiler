d0d260522ff38f7743d9c3ec98dc491864dcceef
Ayush Agarwal <f20190652@pilani.bits-pilani.ac.in>
2023-02-23 22:26:45 +0530

    Grammar Completed

--- parser.c
+++ parser.c
@@ -3,18 +3,34 @@
 int noOfLines(FILE* fptr){
     char* strtok_result;
     int linecount = 0;
-    char buffer[100];
+    fseek (fptr , 0 , SEEK_END);
+    long fptr_size = ftell (fptr);
+    rewind (fptr);
+    char buffer[fptr_size];
+    size_t result;
+    result = fread (buffer,1,fptr_size,fptr);
     strtok_result = strtok(buffer, "\n");
     while(strtok_result!=NULL){
-        strtok_result = strtok(buffer,"\r\n");
-        printf("%s\n",buffer);
         linecount++;
+        strtok_result = strtok(NULL,"\n");
     }
     return linecount;
 }
 
 void populateGrammar(FILE* fptr,ruleLL rules[]){
-
+    char* strtok_result;
+    int linecount = 0;
+    fseek (fptr , 0 , SEEK_END);
+    long fptr_size = ftell (fptr);
+    rewind (fptr);
+    char buffer[fptr_size];
+    size_t result;
+    result = fread (buffer,1,fptr_size,fptr);
+    strtok_result = strtok(buffer, "\n");
+    while(strtok_result!=NULL){
+        linecount++;
+        strtok_result = strtok(NULL,"\n");
+    }
 }
 
 void printArray(ruleLL rules[],int noOfRules){
