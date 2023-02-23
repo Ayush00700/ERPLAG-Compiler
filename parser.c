a6e90d5b3933584ddc5bf68a24e04d4fdbc71335
Ayush Agarwal <f20190652@pilani.bits-pilani.ac.in>
2023-02-23 17:01:39 +0530

    No of Lines in Grammar

--- parser.c
+++ parser.c
@@ -1,13 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "parser.h"
+
+int noOfLines(FILE* fptr){
+    char* strtok_result;
+    int linecount = 0;
+    char buffer[100];
+    strtok_result = strtok(buffer, "\n");
+    while(strtok_result!=NULL){
+        strtok_result = strtok(buffer,"\n");
+        linecount++;
+    }
+    return linecount;
+}
+
+void populateGrammar(FILE* fptr,ruleLL rules[]){
+
+}
+
+void printArray(ruleLL rules[],int noOfRules){
+    for(int i=0;i<noOfRules;i++){
+        
+    }
+}
+
 
 int main(){
-    int noOfRules = 10;
-    int rules[noOfRules];
+    FILE* fptr = fopen("grammar.txt","r+");
+    if(fptr==NULL){
+        perror("Error opening file");
+        exit(1);
+    }
+    int lines = noOfLines(fptr);
+    printf("No of Lines %d",lines);
+    ruleLL rules[lines];
+    populateGrammar(fptr,rules);
+    fclose(fptr);
+    printArray(rules,lines);
 }
-void populateGrammar(FILE* fptr){
-    
-}
+
 
 
--- parser.h
+++ parser.h
@@ -1,17 +1,33 @@
-#include "tokeninfo.h"
 #include "lexer.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-enum Boolean {True,False};
-void populateGrammar(FILE* fptr);
+
+typedef enum Boolean {False,True}Boolean;
+
+
 typedef struct ruleNode{
     token_info* nodeInfo; 
     Boolean isTerminal;
     struct ruleNode* nextNode;
 }ruleNode;
 
+typedef struct FirstSet{
+    token_type type;
+    struct FirstSet* next;
+}FirstSet;
+
+typedef struct FollowSet{
+    token_type type;
+    struct FollowSet* next;
+}FollowSet;
+
 typedef struct ruleLL{
     ruleNode head;
-    
+    int lineNo;
+    FirstSet firstSets;
+    FollowSet followSets;
 }ruleLL;
+
+void populateGrammar(FILE* fptr,ruleLL rules[]);
+int noOfLines(FILE *fptr);
