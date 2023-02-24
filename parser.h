9fc07139e5bb579b0c20e874dec5a7b74990b2d5
Ayush Agarwal <f20190652@pilani.bits-pilani.ac.in>
2023-02-24 06:11:46 +0530

    created enum and strings for NT

--- parser.h
+++ parser.h
@@ -5,7 +5,7 @@
 
 typedef enum Boolean {False,True}Boolean;
 
-char* arr_NonT[100] = {"<prog>","<modDecs>","<othermods>","<driver>",
+char* arr_NonT[65] = {"<prog>","<modDecs>","<othermods>","<driver>",
 "<modDec>","<mod>","<modDef>","<ip_list>","<ret>","<op_list>","<dataType>",
 "<N1>","<type>","<N2>","<range_arrays>","<index_arr>","<stmts>","<stmt>",
 "<iostmt>","<simplestmt>","<declarestmt>","<condstmt>","<iterstmt>",
@@ -14,9 +14,18 @@ char* arr_NonT[100] = {"<prog>","<modDecs>","<othermods>","<driver>",
 "<lvalueARRstmt>","<exp>","<element_index_with_expressions>","<sign>",
 "<optional>","<idList>","<N3>","<arithOrboolExp>","<U>","<unary_op>",
 "<new_NT>","<arithExp>","<AnyTerm>","<N4>","<logOp>","<N5>","<relOp>",
-"<term>","<N6>","<op1>","<factor>","<N7>","<op2>","<arr_element>"};
+"<term>","<N6>","<op1>","<factor>","<N7>","<op2>","<N_11>","<arr_element>",
+"<sign>","<N_11>","<arrExpr>","<N_10>","<arrTerm>","<arr_N4>","<arrFactor>","<arr_N5>","<caseStmts>","<default>","<N9>","<value>","<range_forloop>","<index_forloop>","<new_index_forloop>","<sign_forloop>"};
 
-typedef enum NT{prog}NT;
+typedef enum NT{prog,modDecs,othermods,driver,modDec,mod,modDef,ip_list,ret,op_list,dataType,N1,type,N2,range_arrays,index_arr,stmts,stmt,
+iostmt,simplestmt,declarestmt,condstmt,iterstmt,
+var_print,boolconst,id_num_rnum,arr_ele_print,new_index,
+P1,assignstmt,modReusestmt,whichstmt,lvalueIDstmt,
+lvalueARRstmt,exp,element_index_with_expressions,sign,
+optional,idList,N3,arithOrboolExp,U,unary_op,
+new_NT,arithExp,AnyTerm,N4,logOp,N5,relOp,
+term,N6,op1,factor,N7,op2,N_11,arr_element,
+sign,N_11,arrExpr,N_10,arrTerm,arr_N4,arrFactor,arr_N5,caseStmts,default,N9,value,range_forloop,index_forloop,new_index_forloop,sign_forloop}NT;
 
 typedef struct ruleNode{
     token_info* nodeInfo; 
