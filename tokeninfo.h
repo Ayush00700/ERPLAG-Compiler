e52588704e0884c510a1a380f7b9e7b9f931b433
f2019057@pilani.bits-pilani.ac.in <f20190572@pilani.bits-pilani.ac.in>
2023-02-25 15:30:35 +0530

    populate grammar fn

--- tokeninfo.h
+++ tokeninfo.h
@@ -1,16 +1,11 @@
 #include <stdlib.h>
+#include <string.h>
 
-int arr_keywords_size = 58;
+#define MAX_LEN 20
+#define TABLE_SIZE 101
 
-char* arr_keywords[58] = {
-"ERROR","ID","NUM","RNUM",
-"AND","OR","TRUE","FALSE",
-"PLUS","MINUS","MUL","DIV",
-"LT","LE","GE","GT","EQ","NE",
-"DEF","ENDDEF","DRIVERDEF","DRIVERENDDEF",
-"COLON","RANGEOP","SEMICOL","COMMA",
-"ASSIGNOP","SQBO","SQBC","BO","BC",
-"COMMENTMARK",
+
+char* arr_keywords[26] = {
 "integer","real","boolean","of",
 "array","start","end","declare",
 "module","driver","program","get_value",
@@ -19,22 +14,23 @@ char* arr_keywords[58] = {
 "case","break","default","while"
 };
 
-typedef enum token_type{ //TODO Quotation marks
-ERROR,ID,NUM,RNUM,
-AND,OR,TRUE,FALSE,
-PLUS,MINUS,MUL,DIV,
-LT,LE,GE,GT,EQ,NE,
-DEF,ENDDEF,DRIVERDEF,DRIVERENDDEF,
-COLON,RANGEOP,SEMICOL,COMMA,
-ASSIGNOP,SQBO,SQBC,BO,BC,
-COMMENTMARK,
-INTEGER,REAL,BOOLEAN,OF,
-ARRAY,START,END,DECLARE,
-MODULE,DRIVER,PROGRAM,GET_VALUE,
-PRINT,USE,WITH,PARAMETERS,TAKES,
-INPUT,RETURNS,FOR,IN,SWITCH,
-CASE,BREAK,DEFAULT,WHILE
-}token_type;
+char* arr_keywords_upper[26] = {
+"INTEGER","REAL","BOOLEAN","OF",
+"ARRAY","START","END","DECLARE",
+"MODULE","DRIVER","PROGRAM","GET_VALUE",
+"PRINT","USE","WITH","PARAMETERS","TAKES",
+"INPUT","RETURNS","FOR","IN","SWITCH",
+"CASE","BREAK","DEFAULT","WHILE"
+};
+
+
+typedef struct hash_table_contents{
+
+char* lexeme ;
+char* tk_type;
+
+}hash_table_contents;
+
 
 typedef union data{
     int num;
@@ -42,7 +38,7 @@ typedef union data{
 }data;
 
 typedef struct token_info{
-    token_type type;
+    char type[20];
     int line_no;
     char* lexeme;
     data values;
