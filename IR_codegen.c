8f4a63a55e503c613e88ba04f517d9614e9a469a
A.Sudarshan <66859664+ASudu@users.noreply.github.com>
2023-04-02 18:58:38 +0530

    added few basic functionalities

--- IR_codegen.c
+++ IR_codegen.c
@@ -1,24 +1,76 @@
 #include <string.h>
 #include "IR_codegen.h"
 
-typedef struct ir_code
+typedef enum OPCODE
 {
+    ASSIGN,
+    UNARY_PLUS,
+    UNARY_MINUS,
+    GET_VALUE,
+    PRINT,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    LT,
+    LE,
+    GT,
+    GE,
+    EQ,
+    NEQ,
+    OR,
+    AND,
+    FN_CALL
+}OPCODE;
+
+typedef struct ir_code_node
+/* The entry will be of the following form
+    +------------+----------+----------+-----------+
+    |  operator  |  result  |  left_op |  right_op |
+    +------------+----------+----------+-----------+
+*/
+{
+    OPCODE operator;
     char* result;
     char* left_op;
-    char* operator;
     char* right_op;
     struct ir_code* next;
+}ir_code_node;
+
+typedef struct ir_code
+{
+    ir_code_node* head;
+    ir_code_node* tail;
 }ir_code;
 
 ir_code* global_ir_code;
 
-void initialize_ir_code(){
+void initialize_ir_code()
+/* This function initializes the Quadruple to store the intermediate 3 address code*/
+{
     
     global_ir_code = (ir_code*)malloc(sizeof(ir_code));
-    global_ir_code->left_op = 
-    global_ir_code->next = NULL;
+    global_ir_code->head = global_ir_code->tail = NULL;
 }
-int main(){
+
+void add_entry(ir_code* list, ir_code_node* entry)
+/* This function adds entry to the end of the list*/
+{
+    if(list->head = NULL)
+        list->head = list->tail = entry;
+
+    else if(list->head == list->tail)
+        list->head->next = list->tail = entry;
+    
+    else
+    {
+        list->tail->next = entry;
+        list->tail = entry;
+    }  
+}
+
+int main()
+{
     initialize_ir_code();
     return 0;
 }
