d72a0cdfe288d0b657340558fa27969fb1488532
YasHGoyaL27 <94045050+YasHGoyaL27@users.noreply.github.com>
2023-04-12 17:56:12 +0530

    Delete ast.h

--- ast.h
+++
@@ -1,7 +0,0 @@
-#include "parser.h"
-
-extern ast_node* copier(ast_node* temp);
-extern ast_node* create_ast(treeNodes* root);
-extern void change_tree(treeNodes* root);
-extern void create_abstract_tree();
-extern void recursive_print_tree(ast_node* root,int listcount,FILE* fp);
